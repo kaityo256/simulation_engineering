e4e61f27a9dae6cfe88c993adf7435ff53f492a3
H. Watanabe <kaityo@users.sourceforge.jp>
2022-04-07 02:12:14 +0900

    update

--- 03_mc_advanced/bias/bias.py
+++ 03_mc_advanced/bias/bias.py
@@ -1,21 +1,20 @@
 import numpy as np
 
-N = 2**16
-
 np.random.seed(1)
+N = 2**16
 a = np.random.randint(1, 7, (N))
+
+
 def calc(m):
-    b = int(N/m)
+    b = int(N / m)
     z = np.zeros(b)
     for i in range(b):
-        z[i] = np.average(a[i*m:(i+1)*m])
-    z = 1.0/z
+        z[i] = np.average(a[i * m:(i + 1) * m])
+    z = 1.0 / z
     ave = np.average(z)
-    err = np.std(z)/np.sqrt(len(z))
+    err = np.std(z) / np.sqrt(len(z))
     print(f"{m} {ave} {err}")
 
 
-for m in [4,8,16,32,64, 128]:
+for m in [4, 8, 16, 32, 64, 128]:
     calc(m)
-
-
