07705048c1c2639a51d86c3f0c76262f7b0480fb
H. Watanabe <kaityo@users.sourceforge.jp>
2022-04-08 00:47:52 +0900

    renames dir

--- 03_mc_advanced/case3/langevin2.plt
+++
@@ -1,9 +0,0 @@
-set term png
-set out "langevin2.png"
-
-set xla "t"
-set yla "v"
-
-unset key
-
-p "langevin2.dat" w e pt 6 lc "blue", 0 lt 1 lc "black"
--- 03_mc_advanced/errorbar/errorbar.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-np.random.seed(2)
-for i in range(10):
-    x = 1.0 + i * 0.1
-    y = np.random.normal(loc=1.0+x*0.1, scale=0.1, size=(100))
-    ave = np.average(y)
-    std = np.std(y)/np.sqrt(len(y))
-    print(f"{x} {ave} {std}")
